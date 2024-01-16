8f0a276646dc1800dcbc2606f805408dfdbc6b91
devel@mauriciocap.com <devel@mauriciocap.com>
2024-01-16 17:02:55 -0500

    OK: version limpia para prod

---
+++ src/80prod.py
@@ -0,0 +1,46 @@
+#INFO: los comandos que queremos en la version productiva
+#XXX: generalizar/hacer extensible
+
+import os
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+from typing import Optional
+
+import discord
+from discord import app_commands
+from dolar import priceAsText
+
+#OjO! los canales NO EXISTEN hasta que tienen su primer mensaje (get_channel devuelve NONE)
+REPORT_CH_URL="https://discord.com/channels/1022540900708143195/1196556578120290455" #U: copie mirando canal de reporte
+GUILD_ID= REPORT_CH_URL.split('/')[-2]
+REPORT_CH_ID= int(REPORT_CH_URL.split('/')[-1])
+print(f"GUILD {GUILD_ID} REPORT_CH={REPORT_CH_ID}")
+
+MY_GUILD = discord.Object(id=GUILD_ID)  # replace with your guild id
+
+class MyClient(discord.Client):
+	def __init__(self, *, intents: discord.Intents):
+		super().__init__(intents=intents)
+		self.tree = app_commands.CommandTree(self)
+
+	async def setup_hook(self):
+		# This copies the global commands over to your guild.
+		self.tree.copy_global_to(guild=MY_GUILD)
+		await self.tree.sync(guild=MY_GUILD)
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+	print(f'Logged in as {client.user} (ID: {client.user.id})')
+	print('------')
+
+@client.tree.command()
+async def dolar(interaction: discord.Interaction):
+	"""dolar price"""
+	await interaction.response.send_message(priceAsText())
+
+print(TOKEN)
+client.run(TOKEN)
