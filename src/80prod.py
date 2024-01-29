7147b796ef564a9f17f443f8159214edaf5a03e8
devel@mauriciocap.com <devel@mauriciocap.com>
2024-01-29 13:25:54 -0500

    OK: guardar recorte

--- src/80prod.py
+++ src/80prod.py
@@ -9,7 +9,11 @@ from typing import Optional
 
 import discord
 from discord import app_commands
+
 from dolar import priceAsText
+from util.db_cx_async import db_init, save_instance
+from charlas_app.models import Recorte
+
 
 #OjO! los canales NO EXISTEN hasta que tienen su primer mensaje (get_channel devuelve NONE)
 REPORT_CH_URL= cfg_for('DISCORD_REPORT_CHANNEL_URL') #U: copie mirando canal de reporte #XXX:MULTISERVIDOR?
@@ -26,6 +30,7 @@ class MyClient(discord.Client):
 
 	async def setup_hook(self):
 		# This copies the global commands over to your guild.
+		await db_init() #XXX:replace with alembic?
 		self.tree.copy_global_to(guild=MY_GUILD)
 		await self.tree.sync(guild=MY_GUILD)
 
@@ -47,11 +52,25 @@ async def dolar(interaction: discord.Interaction):
 @client.tree.context_menu(name='Coleccionar', guild= MY_GUILD)
 async def testimonio(interaction: discord.Interaction, message: discord.Message):
 	"""XXX: DOC save data"""
-	d= {'txt': message.content, 'de': message.author, 'fh': message.created_at, 'msg_id': message.id, 'user_save': interaction.user.name}
-	print(f"TESTIMONIO {d}") #XXX: implementar 
-	await interaction.response.send_message(
-		f'Guarde el testimonio (simulado, ver consola del bot)', ephemeral=True
-	)
+	result= "ERROR: message was not saved" #DFLT
+	try:
+		d= {
+			'txt': message.content, 
+			'author_id': str(message.author.id), 
+			'author_name': message.author.name, 
+			'msg_dt': message.created_at, 
+			'msg_id': str(message.id), 
+			'saved_by_id': str(interaction.user.id),
+			'saved_by_name': interaction.user.name,
+		}
+		print(f"RECORTE {d}") 
+		recorte= Recorte.model_validate(d);
+		await save_instance(recorte)
+		result="Recorte guardado!" #XXX: link?
+	except Exception as ex:
+		print("ERROR recorte",d,ex)
+
+	await interaction.response.send_message( result, ephemeral=True)
 
 @client.tree.command()
 async def xtest(interaction: discord.Interaction):
--- src/charlas_app/models.py
+++ src/charlas_app/models.py
@@ -11,10 +11,13 @@ TstrNoVacia= constr(strip_whitespace=True, min_length=1) #A: type alias
 class Recorte(SQLModel, table=True): #U: Mensaje guardado de Discord Boton
 	id: Optional[int] = Field(default=None, primary_key=True) #XXX: campos como en bot
 	created_at: datetime = Field( default_factory=datetime.utcnow, ) #XXX: sacaria el default
+	saved_by_name: str 
+	saved_by_id: str 
+	msg_dt: datetime
 	msg_id: str
-	user: str
+	author_id: str
+	author_name: str
 	txt: str 
-	user_save: str 
 
 
 
